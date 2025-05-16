7bb9c2e2614a62973764fd24617366e5d87616a4
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-05-16 09:36:27 -0300

    #4 feat: criação dos repositories

---
+++ event/src/main/java/com/cqrs/event/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.cqrs.event.repositories;
+
+import com.cqrs.event.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
