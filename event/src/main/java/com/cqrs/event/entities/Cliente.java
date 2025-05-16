86359d2bec88394c7fe63a76e0ffa5c37f36035c
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-05-16 09:13:16 -0300

    #2 feat: criação das entidades e enum

---
+++ event/src/main/java/com/cqrs/event/entities/Cliente.java
@@ -0,0 +1,22 @@
+package com.cqrs.event.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="cliente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String password;
+}
---
+++ event/src/main/java/com/cqrs/event/enums/StatusPagamento.java
@@ -0,0 +1,8 @@
+package com.cqrs.event.enums;
+
+public enum StatusPagamento {
+    PAGO,
+    EM_PROCESSAMENTO,
+    CANCELADO,
+    NAO_PROCESSADO
+}
